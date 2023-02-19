423fbf25ff323b612991640a4c5d70a010b19801
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-02-19 22:28:07 +0100

    change name of home path

--- src/App.js
+++ src/App.js
@@ -302,7 +302,7 @@ class App extends Component {
 {/*End news bar*/}
           <PopupModal />
           <Switch>
-            <Route exact path="/" component={Home} />
+            <Route exact path="/Cultural-website" component={Home} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/profile" component={Profile} />
