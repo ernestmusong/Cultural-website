cae9f1b13240428349969cb9d16f2c915e75b01b
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-02-19 20:11:51 +0100

    Updates

---
+++ precache-manifest.a14d7f8984d2cd11df34b19b6fad640a.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b00be912aa0a48e52afc73a50d2705b",
+    "url": "/Cultural-website/index.html"
+  },
+  {
+    "revision": "b0520f68c3e77b5c95d4",
+    "url": "/Cultural-website/static/css/2.aed5e149.chunk.css"
+  },
+  {
+    "revision": "128ad714ed2e136ffa35",
+    "url": "/Cultural-website/static/css/main.57f80984.chunk.css"
+  },
+  {
+    "revision": "b0520f68c3e77b5c95d4",
+    "url": "/Cultural-website/static/js/2.cc32a54f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Cultural-website/static/js/2.cc32a54f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "128ad714ed2e136ffa35",
+    "url": "/Cultural-website/static/js/main.6dcc82cf.chunk.js"
+  },
+  {
+    "revision": "770afcebc6bec21f048b",
+    "url": "/Cultural-website/static/js/runtime-main.6ee463e8.js"
+  },
+  {
+    "revision": "91b4e95e2ddae48b78a9c8c0b6bee91f",
+    "url": "/Cultural-website/static/media/Dang-Brita.91b4e95e.jpeg"
+  },
+  {
+    "revision": "8f130b32d37e7fca34101dab51583cba",
+    "url": "/Cultural-website/static/media/back-to-school.8f130b32.jpeg"
+  },
+  {
+    "revision": "3c19cf4b64f8f553ccabc8c35ae15e43",
+    "url": "/Cultural-website/static/media/becuda-logo.3c19cf4b.png"
+  },
+  {
+    "revision": "f98d5bd29ecb7859fa27cb8460f31e19",
+    "url": "/Cultural-website/static/media/chief-img.f98d5bd2.jpeg"
+  },
+  {
+    "revision": "f28a5ee2892255264557a1b2ca284dc1",
+    "url": "/Cultural-website/static/media/dominic-gce.f28a5ee2.jpg"
+  },
+  {
+    "revision": "8ee1919141cba622ff594dc0224bfd4f",
+    "url": "/Cultural-website/static/media/eleven.8ee19191.jpeg"
+  },
+  {
+    "revision": "580a3dae9152bffa8a51f837e0c15826",
+    "url": "/Cultural-website/static/media/hero-image.580a3dae.jpeg"
+  },
+  {
+    "revision": "e3885a7d57e035af106aff0a958f98bb",
+    "url": "/Cultural-website/static/media/jimwel.e3885a7d.jpeg"
+  },
+  {
+    "revision": "1210c89835d6b3227c01645205ea65c0",
+    "url": "/Cultural-website/static/media/kalv.1210c898.jpeg"
+  },
+  {
+    "revision": "493b50e229cdb7da4807abc0c95c44b0",
+    "url": "/Cultural-website/static/media/kum-godlove.493b50e2.jpg"
+  },
+  {
+    "revision": "de6859ec90b364bd9b94e8638edb7ef6",
+    "url": "/Cultural-website/static/media/kum-herny.de6859ec.jpg"
+  },
+  {
+    "revision": "8c8fff329585bf7406d832fa0c5f3521",
+    "url": "/Cultural-website/static/media/mendi-georgette.8c8fff32.jpeg"
+  },
+  {
+    "revision": "ed4583a029676bbe779215fc5364f4ba",
+    "url": "/Cultural-website/static/media/radivin.ed4583a0.jpeg"
+  },
+  {
+    "revision": "fc2c257a556729f9289ad0f5c774183d",
+    "url": "/Cultural-website/static/media/water.fc2c257a.jpg"
+  },
+  {
+    "revision": "a1af8518e058397514657e9a0ee52802",
+    "url": "/Cultural-website/static/media/web-project.a1af8518.jpeg"
+  }
+]);
