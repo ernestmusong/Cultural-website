b8607cd256e764516d0cd891a8a35c20a4b305b9
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-10-24 16:31:36 +0100

    Add some styling to dashboard

--- src/components/Dashboard/Dashbord.js
+++ src/components/Dashboard/Dashbord.js
@@ -1,21 +1,9 @@
 import React from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import '../styles/Dashboard.css';
-import {
-  FaTwitter, FaFacebookF, FaVimeoV, FaPinterestP,
-} from 'react-icons/fa6';
-import { TfiGoogle } from 'react-icons/tfi';
-import logo from '../assets/Frame.png';
+import { FaBookOpen } from 'react-icons/fa6';
 import MobileNavigation from './MobileNavigation';
 
-const links = [
-  { path: '/my-contributions', text: 'My contributions' },
-  { path: '/make-reservations', text: 'Reserve a car', id: 'border' },
-  { path: '/my-reservations', text: 'My reservations' },
-  { path: '/add-car', text: 'Add car' },
-  { path: '/delete-car', text: 'Delete car' },
-];
-const currentYear = new Date().getFullYear();
 const NavigationPanel = () => {
   const navigate = useNavigate();
   const logout = () => {
@@ -25,20 +13,18 @@ const NavigationPanel = () => {
   };
   return (
     <>
-      <aside className="navigation-panel">
+      <aside className="sidebar">
         <nav className="navbar">
           <ul>
-            {links.map((link) => (
-              <li key={link.text}>
-                <NavLink
-                  to={link.path}
-                  className={({ isActive }) => (isActive ? 'active' : 'normal')}
-                  id={link.id}
-                >
-                  {link.text}
-                </NavLink>
-              </li>
-            ))}
+            <li  >
+              <FaBookOpen />
+              <NavLink
+                to='/my-contributions'
+                className={({ isActive }) => (isActive ? 'active' : 'normal')}
+              >
+                my contributions
+              </NavLink>
+            </li>
           </ul>
         </nav>
         <button type="button" className="btn" onClick={() => logout()}>Log out</button>
