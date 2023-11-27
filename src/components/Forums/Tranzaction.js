00319cfae27b3f8e821971a8e7b846e976a36b7d
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-11-27 13:20:12 +0100

    Change table layout on transactions

--- src/components/Forums/Tranzaction.js
+++ src/components/Forums/Tranzaction.js
@@ -4,25 +4,22 @@ import PropTypes from 'prop-types';
 const Tranzaction = ({ member }) => {
   const {
     savings,
-    name,
     id,
   } = member;
-  const amounts = savings.map((s) => s.amt);
-  const total = amounts.reduce((a, b) => a + b);
   return (
     <tr className="bg-light" key={id}>
-      <td className="text-capitalize">
-        {name}
-      </td>
+      {savings.map((s) => (
+        <td key={s.id}>
+          {s.date}
+        </td>
+      ))}
       {savings.map((s) => (
         <td key={s.id}>
           {s.amt}
+          {' '}
           cfa
         </td>
       ))}
-      <td className="text-capitalize">
-        {total}
-      </td>
     </tr>
   );
 };
