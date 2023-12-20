467bcebd6e11d5a9ee64225d1c438900795a69ec
Musong Ernest Akeh <ernestakeh@gmail.com>
2023-12-20 06:34:15 +0100

    Make changes to origin

--- src/components/Culture/AboutBefang/Families.js
+++ src/components/Culture/AboutBefang/Families.js
@@ -20,6 +20,20 @@ const Families = () => (
       The first five family groups are refered to as Kibien - meaning corners, while the last three
       are refered to as Buo-Ketsm or Matern - meaning center.
     </p>
+    <p className="text-bold">
+      Women who commited suicide:
+      {' '}
+      <br />
+      These women are often refered to as
+      {' '}
+      <span className="text-uppercase" style={{ color: 'Var(--lightGreen)' }}>ba-ngon-uwong</span>
+      {' '}
+      . Ba-ngon-uwong were a group of women who, out of depression, anger, and
+      frustrattion committed suicide by voluntrilly holding themselves
+      together, and throwing themselves deep down a valley after the death of their husbands.
+      It is unclear what led to the death of their husbands, but some people hold that It
+      was a German invasion during the German annexation of Cameroon.
+    </p>
   </>
 );
 
